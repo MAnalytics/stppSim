5ce2a1232d8ae65a36e35077fbcfa0a22ba9d47a
geoMADE <m.adepeju@mmu.ac.uk>
2021-08-30 18:48:33 +0100

    update data

--- R/data.R
+++ R/data.R
@@ -6,3 +6,12 @@
 #'   \item y: y coordinate
 #'     }
 "poly"
+
+#' A rectangular boundary coordinates
+#'
+#' @format A dataframe containing one variable:
+#' \itemize{
+#'   \item x: x coordinate
+#'   \item y: y coordinate
+#'     }
+"regular_poly"
