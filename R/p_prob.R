eaf73456b1e7139ecbc567d414bd21461aacf72f
geoMADE <m.adepeju@mmu.ac.uk>
2021-08-30 18:48:56 +0100

    update p_prob.R

--- R/p_prob.R
+++ R/p_prob.R
@@ -1,56 +1,81 @@
 #' @title Pareto Probability distribution
-#' @description Given a number of points, this function
-#' assign probability values (signifying
-#' their strength) to the points in accordance with
-#' the pareto principle (i.e. 80% of the events
-#' come from 20% of the locations).
-#' @param npoints (an integer) Number of points
-#' @usage p_prob(npoints)
+#' @description Given a specified number of points \code{n},
+#' this function generates an \code{n} probability values
+#' in accordance with the Pareto principle.
+#' @param npoints (an integer) Number of points. Default is
+#' \code{50}.
+#' @param p_ratio (an integer) The smaller of the
+#' two terms of a Pareto ratio. For example, for a 20:80
+#' ratio, `p_ratio` will be \code{20}. Default value is
+#' \code{30}. Input values must be \code{10}, \code{20},
+#' \code{30}, \code{40}, or \code{50}. The 'p_ratio'
+#' determines the proportion of points that are the most
+#' dominant event generators.
+#' @usage p_prob(npoints,  p_ratio = 30)
 #' @examples
 #' @details
 #' @return Returns the global temporal pattern
 #' @references
 #' @importFrom magrittr %>%
-#'
-#'
 #' @export
 
-p_prob <- function(npoints =  50){
+p_prob <- function(npoints =  50, p_ratio = 30){
 
   mutate <- area <- total <-  NULL
 
+  #Table showing the Pareto ratio value and the corresponding
+  #pre-determined
+  #exponential constant for different
+  #Pareto ratio
+  exp_param_table <- data.frame(ratio=seq(10, 50, 10),
+                                constant = c(2.3, 0.8, 0.39,
+                                             0.17, 0.004))
+
+  if(!p_ratio %in% exp_param_table$ratio){
+    stop(paste(" 'p_ratio' has to be one of the",
+               "following values: [10, 20, 30, 40, 50]", sep = " "))
+  }
+
+#}
+
   n <- npoints
 
-  q <- 0.8/(n/10)       #+#decay (denting) parameter
+  #get the constant
+  constant<-exp_param_table[which(exp_param_table$ratio == p_ratio),
+                              2]
+
+  #determine q, the decay (expo. denting) parameter
+  q <- constant/(n/10)
   #plot(y,exp(q*y))
 
-  a_table <- NULL #area table
+  p_table <- NULL #area table
   #steps <- n
 
   for(i in 1:n){ #i<-0.5
 
     if(i == 1){
       area_i <- exp(q*i) - exp(q*(0))
-      a_table <- c(a_table, area_i)
+      p_table <- c(p_table, area_i)
     }
     if(i != 1){
       area_i <- exp(q*i) - exp(q*(i-1))
-      a_table <- c(a_table, area_i)
+      p_table <- c(p_table, area_i)
     }
   }
 
-    a_table <- data.frame(sn=1:n, area=a_table)
+    p_table <- data.frame(sn=1:n, area=p_table)
 
-    a_table <- a_table %>%
+    p_table <- p_table %>%
       mutate(total=sum(area))%>%
       mutate(prob=round(area/total, digits = 8))
 
-  return(a_table)
+  return(p_table)
+
 }
 
 
 # 0.2*50
-# sum(a_table[1:40])/sum(a_table[1:50])
+# sum(p_table[1:40])/sum(p_table[1:50])
 
-0.7 * 50# sum(a_table[41:50])/sum(a_table[1:50])
+#0.7 * 50# sum(p_table[41:50])/sum(p_table[1:50])
 
