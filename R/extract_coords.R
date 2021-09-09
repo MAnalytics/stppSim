4e30f04479cdd1316d06f0677d4448f0a9c525c2
geoMADE <m.adepeju@mmu.ac.uk>
2021-09-09 07:28:36 +0100

    upload

---
+++ R/extract_coords.R
@@ -0,0 +1,22 @@
+#' @title Extracting coordinates of a polygon boundary
+#' @description Given a polygon object, the goal is to extract
+#' the coordinates of the edges of the boundary.
+#' @param poly (a spatialPolygons, spatialPolygonDataFrames,
+#' or a simple feature polygon). The polygon object must be in a
+#' Cartesian coordinate system.
+#' @usage extract_coords(poly)
+#' @examples
+#' @details
+#' @return Returns the global temporal pattern
+#' @references
+#' @importFrom magrittr %>%
+#' @export
+
+extract_coords <- function(poly){
+
+    #check the geometry of the input
+
+
+    #check the coordinate system
+
+}
