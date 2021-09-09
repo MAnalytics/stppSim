a5fb4d4bd064710ba598a716bcfaee1f03e96f5f
geoMADE <m.adepeju@mmu.ac.uk>
2021-09-09 17:43:51 +0100

    update package

--- R/extract_coords.R
+++ R/extract_coords.R
@@ -10,13 +10,27 @@
 #' @return Returns the global temporal pattern
 #' @references
 #' @importFrom magrittr %>%
+#' @importFrom sf st_as_sf st_coordinates
+#' @importFrom dplyr select
 #' @export
 
 extract_coords <- function(poly){
 
+  X <- Y <- NULL
+
     #check the geometry of the input
+    if(!class(poly)[1] %in% c("SpatialPolygonsDataFrame",
+                             "SpatialPolygons")){
+      stop(paste("Not the required object class!"))
+    }
 
+  #convert object class to simple feature (sf)
+  poly2 <- st_as_sf(poly)
+  poly2 <- data.frame(st_coordinates(poly2))
+  poly2 <- poly2 %>%
+    select(X, Y)
 
-    #check the coordinate system
-
+  return(poly2)
 }
+
+
