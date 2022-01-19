aeb43cd42aef74bf4dcd304eba64f47b7529ee1e
geoMADE <m.adepeju@mmu.ac.uk>
2022-01-19 02:03:25 +0000

    update date_chk

--- R/date_checker.R
+++ R/date_checker.R
@@ -2,23 +2,21 @@
 #' @description Checks if date is in the correct format, i.e.
 #' `yyyy-mm-dd`
 #' @param x A date input
-#' @param format The date format required.
-#' Default value: \code{"%Y-%m-%d"}.
-#' @usage is_date(x, format="%Y-%m-%d")
+#' @param format The date format required, e.g.,
+#' \code{"%Y-%m-%d"}.
+#' @usage date_checker(x, format=NULL)
 #' @examples
-#' @details Returns TRUE or FALSE, to indicate
-#' that the date input is in the correct or incorrect
-#' format, respectively
+#' @details
 #' @return Returns TRUE or FALSE
 #' @references
 #' #https://online.stat.psu.edu/stat510/lesson/6/6.1
 #' @export
 #'
 
-date_checker <- function(x, format = "%Y-%m-%d") {
+date_checker <- function(x, format = NULL) {
 
-  formatted = try(as.Date(x, format), silent = TRUE)
-  is_date = as.character(formatted) == x & !is.na(formatted)  # valid and identical to input
+  formatted <- try(as.Date(x, format), silent = TRUE)
+  is_date <- as.character(formatted) == x & !is.na(formatted)  # valid and identical to input
   is_date[is.na(x)] = NA  # Insert NA for NA in x
 
   return(is_date)
