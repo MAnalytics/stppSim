c91f87b29a45445a4439e48a7926cef68dd4cd86
geoMADE <m.adepeju@mmu.ac.uk>
2022-01-21 02:30:12 +0000

    create st_learner

---
+++ R/st_learner.R
@@ -0,0 +1,21 @@
+#' @title Learns spatio-temporal signatures of sample data
+#' @description Learns the spatial and temporal details of
+#' a sample point dataset.
+#' @param stp (dataframe, spatialpoint, or simple feature)
+#' Records of sample point datasets
+#' covering a geographical extent. `stp` will contain
+#' three fields: x, y and t, representing the eastings, northings,
+#' and the time of occurence.
+#' @usage st_learner(stp)
+#' @examples
+#' @details Returns the spatiotemporal details of point datasets
+#' @references https://www.google.co.uk/
+#' @importFrom dplyr select
+#' @export
+st_learner <- function(stp){
+
+
+
+
+
+}
