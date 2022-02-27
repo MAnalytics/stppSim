ab691ecaf72bac0919536f2b6a9f8c52bc51cdc1
geoMADE <m.adepeju@mmu.ac.uk>
2022-02-27 00:44:41 +0000

    Update space_resistance.R

--- R/space_resistance.R
+++ R/space_resistance.R
@@ -1,10 +1,9 @@
-#' @title Space (landscape) resistance raster
+#' @title Space Resistance Raster
 #' @description Build a space resistance map
 #' from one or more shapefiles. Function builds
-#' on functions from package raster- and
+#' on raster- and
 #' SimRIv-packages.
-#' @param shp	either a character string specifying
-#' the shapefile filename or a shapefile object itself.
+#' @param shp	polygon shapefile object.
 #' @param baseRaster if provided, a raster onto which
 #' to stack the given rasterized shapefile.
 #' @param res	the desired pixel resolution of the
@@ -12,23 +11,20 @@
 #' @param binary if TRUE, the shapefile will be rasterized
 #' so that any feature is assigned a value of 0,
 #' and the background 1.
-#' @param field either a number in the range 0 to 1sa, in
+#' @param field either a value in the range 0 to 1sa, in
 #' which case it will be assigned to all pixels covered
 #' by features of the shapefile; or the name of the numeric
-#' shapefile field from which to extract such number; or
+#' field from which to extract such value; or
 #' the name of the factor shapefile field containing classes
-#' to map to resistance values (see mapvalues).
+#' to map to resistance values.
 #' @param mapvalues a named vector specifying the resistance
 #' value mapping from the classes of field.
-#' @param extend set to TRUE to extend baseRaster if the shapefile
-#' has a larger extent. If FALSE, the shapefile will be clipped
-#' to baseRaster's extent.
 #' @usage space_resistance(shp, baseRaster, res, binary = is.na(field),
-#' field = NA, mapvalues = NA, extend = TRUE)
+#' field = NA, mapvalues = NA)
 #' @examples
 #' @details
-#' @return Returns a trace of walker's path, and the
-#' corresponding events.
+#' @return Returns a raster showing space resistance levels
+#' or intensity to event occurrences.
 #' @references
 #' 1. Paul Murrell (2019). rasterize: Rasterize Graphical
 #' Output. R package version 0.1.
@@ -41,16 +37,16 @@
 #' @importFrom SiMRiv resistanceFromShape
 #' @export
 
-space_resistance <- function(shp, baseRaster, res, binary = is.na(field),
+space_resistance <- function(shp, baseRaster, res=100, binary = is.na(field),
                              field = NA,
-                             mapvalues = NA, extend = TRUE){
+                             mapvalues = NA){
 
 
-  space_res <- resistanceFromShape(shp, baseRaster, res, binary = is.na(field)
-                      , field = NA, background = 1, buffer = NA, margin = 0
-                      , mapvalues = NA, extend = TRUE)
+  space_res <- resistanceFromShape(shp = shp, baseRaster = baseRaster, res = res,
+                                   binary = is.na(field), field = NA,
+                                   background = 1, buffer = NA, margin = 0,
+                                   mapvalues = NA, extend = FALSE)
 
   return(space_res)
-
 
 }
