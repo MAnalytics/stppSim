5b9d6a649fbd22365266ead46c1fa15920d18e4f
geoMADE <m.adepeju@mmu.ac.uk>
2022-03-01 17:41:03 +0000

    Update date_checker.R

--- R/date_checker.R
+++ R/date_checker.R
@@ -1,24 +1,39 @@
-#' @title Date Format Checker
-#' @description Checks if date is in the correct format, i.e.
-#' `yyyy-mm-dd`
-#' @param x A date input
-#' @param format The date format required.
-#' Default: NULL.
-#' @usage date_checker(x, format=NULL)
+#' @title Date (Format) Checker
+#' @description Checks if date is in the correct
+#' format.
+#' @param x A vector of date values
+#' @usage date_checker(x)
 #' @examples
-#' @details
+#' date_list_1 <- c("2021-09-12", "2016-xx-02",
+#' "09/08/2012")
+#' date_checker(date_list_1)
+#' #> FALSE (Entries 2 and 3
+#' #are incorrect date inputs)
+#' date_list_2 <- c("2021-09-12", "1998-03-09")
+#' date_checker(date_list_2)
+#' #> TRUE
+#' @details Returns \code{"TRUE"} if all
+#' entries of a vector are date values and
+#' \code{FALSE} if any entries of a vector
+#' is not a date value. The date vector
+#' needs to be in the format: `"yyyy-mm-dd"`.
 #' @return Returns TRUE or FALSE
-#' @references
-#' #https://online.stat.psu.edu/stat510/lesson/6/6.1
 #' @export
 #'
 
-date_checker <- function(x, format = NULL) {
+date_checker <- function(x) {
 
-  formatted <- try(as.Date(x, format), silent = TRUE)
-  is_date <- as.character(formatted) == x & !is.na(formatted)  # valid and identical to input
-  is_date[is.na(x)] = NA  # Insert NA for NA in x
+  is.convertible.to.date <- function(x) !is.na(as.Date(as.character(x),
+                          tz = 'UTC', format = '%Y-%m-%d'))
 
-  return(is_date)
+  is.date <- sapply(list(x), is.convertible.to.date)
+  is.date <- as.character(is.date)
+
+  if("FALSE" %in% is.date){
+    return("FALSE")
+  }
+  if(!"FALSE" %in% is.date){
+    return("TRUE")
+  }
 
 }
