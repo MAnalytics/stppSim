d0c9bcb584b0d2ca47554eb9e9a6370c9d83b498
geoMADE <m.adepeju@mmu.ac.uk>
2022-03-01 19:23:58 +0000

    Update p_prob.R

--- R/p_prob.R
+++ R/p_prob.R
@@ -1,31 +1,34 @@
-#' @title Pareto Probability distribution
-#' @description Given a specified number of points \code{n},
-#' this function generates an \code{n} probability values
-#' in accordance with a specified Pareto ratio.
-#' @param n_origin (an integer) Number of origins. Default is
-#' \code{50}.
+#' @title Pareto (probability) distribution
+#' @description Generates an \code{n} probability
+#' values in accordance with a specified Pareto ratio.
+#' @param n (an integer) Number of data points.
 #' @param p_ratio (an integer) The smaller of the
-#' two terms of a Pareto ratio. For instance, for a
-#' \code{20:80} ratio, `p_ratio` will be \code{20}. Default value is
-#' \code{20}. Input values must be \code{5}, \code{10}, \code{20},
-#' \code{30}, or \code{40}. The 'p_ratio'
-#' determines the proportion of points that are the most
-#' dominant event generators.
-#' @usage p_prob(n_origin,  p_ratio = 30)
+#' terms of a Pareto ratio. For instance, for a
+#' \code{20:80} ratio, `p_ratio` is \code{20}.
+#' Default value is set as
+#' \code{20}. Acceptable `p_ratio` values
+#' are: (\code{5, 10, 20, 30, 40}).
+#' @usage p_prob(n,  p_ratio = 20)
 #' @examples
-#' @details
-#' @return Returns the global temporal pattern
-#' @references https://www.google.co.uk/
+#' p_prob(n = 15,  p_ratio = 20)
+#' @details Produces a list probability values
+#' based on the specified Pareto ratio. Each ratio
+#' term is assigned the proportion of area under
+#' an exponential curve that is equal to
+#' the value of the other ratio term. For example,
+#' for a \code{20:80} ratio, 20% of data points
+#' covers 80% of areas under the curve, and vice
+#' versa.
+#' @return Returns a vector of probability values
 #' @importFrom magrittr %>%
 #' @export
 
-p_prob <- function(n_origin = 50, p_ratio = 30){
+p_prob <- function(n = 50, p_ratio = 20){
 
   mutate <- area <- total <-  NULL
 
   #Table showing the Pareto ratio value and the corresponding
-  #pre-determined
-  #exponential constant for different
+  #pre-determined exponential constant for different
   #Pareto ratio
   exp_param_table <- data.frame(ratio=c(5, seq(10, 40, 10)),
                                 constant = c(6.0, 2.3, 0.8, 0.39,
@@ -36,20 +39,15 @@ p_prob <- function(n_origin = 50, p_ratio = 30){
                "following values: [5, 10, 20, 30, 40]", sep = " "))
   }
 
-#}
-
-  n <- n_origin
 
   #get the constant
-  constant<-exp_param_table[which(exp_param_table$ratio == p_ratio),
-                              2]
+  constant<-exp_param_table[which(exp_param_table$ratio ==
+                                    p_ratio),2]
 
   #determine q, the decay (expo. denting) parameter
   q <- constant/(n/10)
-  #plot(y,exp(q*y))
 
   p_table <- NULL #area table
-  #steps <- n
 
   for(i in 1:n){ #i<-0.5
 
@@ -72,10 +70,3 @@ p_prob <- function(n_origin = 50, p_ratio = 30){
   return(p_table)
 
 }
-
-
-# 0.2*50
-# sum(p_table[1:40])/sum(p_table[1:50])
-
-#0.7 * 50# sum(p_table[41:50])/sum(p_table[1:50])
-
