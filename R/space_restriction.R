4783fc7c4fe3692ab43b6892e18ec4620f7fb579
geoMADE <m.adepeju@mmu.ac.uk>
2022-03-05 05:54:55 +0000

    Update space_restriction.R

--- R/space_restriction.R
+++ R/space_restriction.R
@@ -10,19 +10,22 @@
 #' @param res	the desired pixel resolution of the
 #' raster to be created, when baseMap is not provided.
 #' @param binary if TRUE, the shapefile will be rasterized
-#' so that any feature is assigned a value of 0 (least resistance
-#' to event occurences),
-#' and the background 1 (most resistance to event occurences).
-#' @param field (string) the name of the character
-#' field from which to extract the label of different
-#' feature classes.
-#' @param mapvalues a named vector specifying the resistance
-#' value mapping from the classes of field.
+#' so that any feature is assigned a value of `0`,
+#' and the background is assigned `1`.
+#' @param field A number in the range of \code{[0-1]}
+#' (i.e. resistance values) to
+#' assign to all features covered by `shp`; or
+#' the name of a numeric field to extract such
+#' resistance values for different feature classes.
+#' The resistance value `0` and `1` indicate the
+#' lowest and the highest restrictions, respectively,
+#' to an event occuring within the space occupied
+#' by a feature.
 #' @param background the value in the range 0 and 1
 #' to assign to all pixels that are not covered
 #' by any shapefile feature.
 #' @usage space_restriction(shp, baseMap, res, binary = is.na(field),
-#' field = NA, mapvalues = NA, background = 1)
+#' field = NA, background = 1)
 #' @examples
 #' data(camden_boundary)
 #' restrct_space <- space_restriction(shp = camden_boundary,
@@ -31,11 +34,10 @@
 #' #plot(restrct_space)
 #' #Setting 'restrct_space' raster as basemap, the landuse
 #' #map can now be stacked onto the basemap as follows:
-#' data(camden_landuse)
-#' restrct_Landuse <- space_restriction(shp = camden_landuse,
+#' data(cam_landuse)
+#' restrct_Landuse <- space_restriction(shp = landuse,
 #' baseMap = restrct_space,
-#' res = 20, field = "type", mapvalues = c("Green" = 0.8,
-#' "Leisure" = 1, "Sports" = 0.7), background = 1)
+#' res = 20, field = "rValues2", background = 1)
 #' #plot(restrct_Landuse)
 #' @details Help to create a complete space restriction map
 #' with cell values ranging from 0(lowest) and 1(highest). All
@@ -57,15 +59,13 @@
 #' @export
 
 space_restriction <- function(shp, baseMap, res=20, binary = is.na(field),
-                             field = NA,
-                             mapvalues = NA, background = 1){
+                             field = NA, background = 1){
 
 
   space_res <- resistanceFromShape(shp = shp, baseRaster = baseMap, res = res,
                                    binary = binary, field = field,
                                    background = background, buffer = NA,
-                                   margin = 0,
-                                   mapvalues = mapvalues, extend = FALSE)
+                                   margin = 0,extend = FALSE)
 
   return(space_res)
 
