7dbe2a7919283c00d328ab2f0d9cf36eb8206499
geoMADE <m.adepeju@mmu.ac.uk>
2022-03-12 23:12:20 +0000

    Update data.R

--- R/data.R
+++ R/data.R
@@ -11,32 +11,27 @@
 
 #' @title Records of crimes of Camden Borough of London,
 #' UK, 2021 (Source: https://data.police.uk/data/)
-#' @description 'Theft' and 'Criminal Damage' records
-#' (Source: `police.uk`)
-#' of Camden Borough of London, UK
-#' for the year 2021.
-#' Note: Police.uk data is only accurate to monthly
+#' @description Data comprising 'Theft' and 'Criminal Damage'
+#' records of Camden Borough of London, UK
+#' for the year 2021 (Source: `https://data.police.uk/`).
+#' Note: Police.uk data is aggregated at monthly
 #' scale (`yyyy-mm`). But, the data provided here has been
-#' manipulated to include fake 'days of occurrence'.
-#' So, caution should
+#' disaggregated to daily scale by adding fake
+#' 'daily' stamps (to give `yyyy-mm-dd`). So, caution should
 #' be taken when interpreting the results based on
-#' full date stamp (`yyyy-mm-dd`).
-#' @format A matrix containing three variables
+#' full date.
+#' @format A matrix containing four variables
 #' \itemize{
 #'   \item x: x coordinate
 #'   \item y: y coordinate
 #'   \item date: date of occurence
-#'   \item type: type of crime
+#'   \item type: types of crime
 #'     }
 "camden_crimes"
 
 #' @title A boundary shapefile
 #' @description A boundary shapefile of Camden Borough, London, UK
 #' @format A boundary file (ESRI format)
-#' \itemize{
-#'   \item x: x coordinate
-#'   \item y: y coordinate
-#'     }
 "camden_boundary"
 
 #' @title Landuse shapefile
