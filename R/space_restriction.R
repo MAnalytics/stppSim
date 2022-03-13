4bcfe02acb5bac3184b6c3eed74bd320555e22c0
geoMADE <m.adepeju@mmu.ac.uk>
2022-03-13 04:01:23 +0000

    Update space_restriction.R

--- R/space_restriction.R
+++ R/space_restriction.R
@@ -1,29 +1,34 @@
-#' @title Space restriction map (raster)
-#' @description Build a space restriction map
-#' from one or more shapefiles. A space restriction map
-#' (raster) shows the restrictions (to event occurences)
-#' across space. Function builds
+#' @title Space restriction raster map
+#' @description Builds a space restriction map
+#' from one or more shapefiles. A space restriction
+#' raster map showing the restriction levels
+#' of various features across the landscape.
+#' The function builds
 #' on raster- and SimRIv-packages.
-#' @param shp	polygon shapefile object.
+#' @param shp	shapefile object containing
+#' features to serve as obstructions to the movement
+#' of walkers.
 #' @param baseMap if provided, a raster onto which
-#' to stack the next rasterized shapefile.
+#' to stack the restriction features (`shp`).
 #' @param res	the desired pixel resolution of the
 #' raster to be created, when baseMap is not provided.
 #' @param binary if TRUE, the shapefile will be rasterized
-#' so that any feature is assigned a value of `0`,
-#' and the background is assigned `1`.
-#' @param field A number in the range of \code{[0-1]}
-#' (i.e. resistance values) to
-#' assign to all features covered by `shp`; or
+#' so that all features are assigned a value of `0` (minimum
+#' restriction level),
+#' and the background is assigned `1`
+#' (maximum restriction level).
+#' @param field a number in the range of \code{[0-1]}
+#' (i.e. restriction values) assigned
+#' to all features; or
 #' the name of a numeric field to extract such
-#' resistance values for different feature classes.
-#' The resistance value `0` and `1` indicate the
-#' lowest and the highest restrictions, respectively,
-#' to an event occuring within the space occupied
-#' by a feature.
+#' restriction values for different classes of
+#' feature.
+#' Restriction value `0` and `1` indicate the
+#' lowest and the highest obstructions, respectively.
+#' Default: \code{NULL}.
 #' @param background the value in the range 0 and 1
 #' to assign to all pixels that are not covered
-#' by any shapefile feature.
+#' by any shapefile object.
 #' @usage space_restriction(shp, baseMap, res, binary = is.na(field),
 #' field = NA, background = 1)
 #' @examples
@@ -39,9 +44,11 @@
 #' baseMap = restrct_space,
 #' res = 20, field = "rValues2", background = 1)
 #' #plot(restrct_Landuse)
-#' @details Help to create a complete space restriction map
-#' with cell values ranging from 0(lowest) and 1(highest). All
-#' other area not covered by any features is assigned the value
+#' @details Helps to create a complete space restriction map
+#' with cell values ranging from 0
+#' (`minimum restriction level`) and 1(`maximum restriction
+#' level`). All
+#' other areas not covered by any features are assigned the value
 #' of `background`. When stacking additional features to existing
 #' `baseMap`, only the areas covered by features are updated, while
 #' the remaining areas retain the original values of `baseMap`.
