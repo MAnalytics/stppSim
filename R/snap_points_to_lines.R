5e5971c008ebc1ad52276261bd9ccd5f9b117146
Monsuru Adepeju <m.adepeju@mmu.ac.uk>
2023-07-21 21:24:18 +0100

    Create snap_points_to_lines.R

---
+++ R/snap_points_to_lines.R
@@ -0,0 +1,90 @@
+#' @title Snapping point to network
+#' @description Snaps points to the nearest segment
+#' of a network data.
+#' @param points point data (sf object)
+#' @param lines line/street/road network (sf object)
+#' @param verbose Whether to output processing
+#' messages.
+#' @usage snap_points_to_lines(points, lines,
+#' all_points = FALSE, verbose = FALSE)
+#' @examples
+#'
+#' #get line and point data
+#' load(file = system.file("extdata", "camden.rda",
+#' package="stppSim"))
+#' lines <- camden$lines
+#' pts <- camden$pts
+#' my_points <- snap_points_to_lines(points=pts,
+#' lines=lines,
+#' verbose = FALSE)
+#'
+#' #preview result
+#' #ggplot()+
+#' #geom_sf(data = roads, col = 'red')+
+#' #geom_sf(data = my_points, shape = 1)
+
+#' @details Function snaps points (within 300m)
+#' to the nearest segment on a network. The remaining
+#' points outside 300m buffer are returned in
+#' their original locations (Credit: Michal Kvasnicka)
+#' @return Point (sf object) with adjusted coordinates
+#' to fit on the network data
+#' @export
+#'
+
+snap_points_to_lines <- function(points, lines,
+                                 verbose = FALSE){
+
+  all_points = TRUE
+  dist = 300
+
+  buffer_lines <- function(lines, dist = 300, verbose = FALSE) {
+    if (verbose) message("Creating buffer for lines...")
+    st_buffer(lines, dist = dist) |> st_union()
+  }
+
+  get_points_close_to_lines <- function(points, lines, dist = 300,
+                                        verbose = FALSE) {
+    buff <- buffer_lines(lines, dist = dist, verbose = verbose)
+    if (verbose) message("Finding which points are in buffer...")
+    st_intersects(points, buff, sparse = FALSE)
+  }
+
+
+  ##snap_points_to_lines <- function(points, lines, dist = 300,
+                                   ##all_points = FALSE, verbose = FALSE) {
+
+    inside <- get_points_close_to_lines(points, lines, dist = dist,
+                                        verbose = verbose)
+    buff_points <- points[inside, ]
+    if (verbose) message("Finding nearest features...")
+    nf <- st_nearest_feature(buff_points, lines)
+    if (verbose) message("Finding nearest points...")
+    np <- st_nearest_points(buff_points, lines[nf, ], pairwise = TRUE)
+    np <- st_cast(np, "POINT")[c(FALSE, TRUE)]
+    if (verbose) message("Adding attributes...")
+    out <- st_drop_geometry(buff_points)
+    out$geometry <- np
+    out <- st_as_sf(out)
+    if (all_points) {
+      if (verbose) message("Adding points outside buffer...")
+      out$valid <- TRUE
+      if(length(which(out$valid=="TRUE"))!=nrow(points)){
+      outside <- points[!inside, ]
+      outside$valid <- FALSE
+      out <- dplyr::bind_rows(out, outside)
+      }
+      if(length(which(out$valid=="TRUE"))==nrow(points)){
+      out <- out
+      }
+    }
+    return(out)
+  ##}
+
+}
+
+
+
+
+
+
